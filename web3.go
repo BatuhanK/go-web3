88758c7f6e822b9457bf8418ccbd1bc981962c5b
Batuhan KATIRCI <katircib@gmail.com>
2022-05-25 18:51:15 +0300

    fix method

--- web3.go
+++ web3.go
@@ -14,45 +14,12 @@ type Web3 struct {
 	c     *rpc.Client
 }
 
-func NewWeb3(provider string) (*Web3, error) {
-	return NewWeb3WithProxy(provider, "")
+func NewWeb3(provider string, headers map[string]string) (*Web3, error) {
+	return NewWeb3WithProxy(provider, "", headers)
 }
 
-func NewWeb3WithProxy(provider, proxy string) (*Web3, error) {
-	c, err := rpc.NewClient(provider, proxy)
-	if err != nil {
-		return nil, err
-	}
-	e := eth.NewEth(c)
-
-	providerLowerStr := strings.ToLower(provider)
-
-	if strings.Contains(providerLowerStr, "ropsten") {
-		e.SetChainId(3)
-	} else if strings.Contains(providerLowerStr, "kovan") {
-		e.SetChainId(42)
-	} else if strings.Contains(providerLowerStr, "rinkeby") {
-		e.SetChainId(4)
-	} else if strings.Contains(providerLowerStr, "goerli") {
-		e.SetChainId(5)
-	} else {
-		e.SetChainId(1)
-	}
-
-	u := utils.NewUtils()
-	w := &Web3{
-		Eth:   e,
-		Utils: u,
-		c:     c,
-	}
-
-	// Default poll timeout 2 hours
-	w.Eth.SetTxPollTimeout(7200)
-	return w, nil
-}
-
-func NewWeb3WithHeaders(provider string, headers map[string]string) (*Web3, error) {
-	c, err := rpc.NewClientWithHeaders(provider, headers)
+func NewWeb3WithProxy(provider, proxy string, headers map[string]string) (*Web3, error) {
+	c, err := rpc.NewClient(provider, proxy, headers)
 	if err != nil {
 		return nil, err
 	}
