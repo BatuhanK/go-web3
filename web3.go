b43db2c35678ac38fb5b3b94da3d0276e23f45ed
Batuhan KATIRCI <katircib@gmail.com>
2022-05-25 18:20:19 +0300

    replace names

--- web3.go
+++ web3.go
@@ -3,9 +3,9 @@ package web3
 import (
 	"strings"
 
-	"github.com/chenzhijie/go-web3/eth"
-	"github.com/chenzhijie/go-web3/rpc"
-	"github.com/chenzhijie/go-web3/utils"
+	"github.com/BatuhanK/go-web3/eth"
+	"github.com/BatuhanK/go-web3/rpc"
+	"github.com/BatuhanK/go-web3/utils"
 )
 
 type Web3 struct {
